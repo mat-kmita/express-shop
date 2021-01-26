532375a7370d6058e9b5c82dc9c02b171426bba7
Mateusz Kmita <mateusz.kmita@mail.com>
2021-01-26 10:56:36 +0100

    Created OrdersProductsRepository

--- repository/index.js
+++ repository/index.js
@@ -188,10 +188,30 @@ class OrdersRepository {
     }
 }
 
+class OrdersProductsRepository {
+    constructor(conn) {
+        this.conn = conn;
+    }
+
+    async getProductsForOrder(orderId) {
+        let query = 'SELECT * FROM orders_products JOIN products ON products.id = orders_products.product_id WHERE orders_products.order_id = $1';
+        let result;
+
+        try {
+            result = this.conn.many(query, [orderId]);
+        } catch(err) {
+            console.log(err);
+        }
+
+        return result;
+    }
+}
+
 module.exports = function(conn) {
     return {
         UserRepository: new UserRepository(conn),
         CategoriesRepository: new CategoriesRepository(conn),
-        OrdersRepository: new OrdersRepository(conn)
+        OrdersRepository: new OrdersRepository(conn),
+        OrdersProductsRepository: new OrdersProductsRepository(conn)
     }
 }
