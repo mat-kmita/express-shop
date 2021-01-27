77ede9fb62c02cfffb19633123d305b14a936b36
Mateusz Kmita <mateusz.kmita@mail.com>
2021-01-27 10:53:44 +0100

    Added new repositories

--- repository/index.js
+++ repository/index.js
@@ -46,66 +46,133 @@ class UserRepository {
         return true;
     } 
 
-    async update(user) {
-        const column_names = {
-            'fistName': 'first_name',
-            'lastName': 'last_name',
-            'street': 'address_street',
-            'buildingNumber': 'address_building_number',
-            'flatNumber': 'address_flat_number',
-            'postalCode': 'address_postal_code',
-            'city': 'address_city'
-        };
-        let text = 'UPDATE users SET ';
+    async getPage(pageNumber, pageLength) {
+        let query = 'SELECT * FROM users ORDER BY id OFFSET $1 ROWS FETCH NEXT $2 ROWS ONLY';
+        let result;
 
-        let queryText = '';
-        for (let field in user) {
-            if (user[field].len > 0) {
-
-            }
+        try {
+            result = await this.conn.manyOrNone(query, [pageLength * (pageNumber - 1), pageLength]);
+        } catch(err) {
+            console.error('Error while retrieving users from database!');
+            console.error(err.message);
+            result = null;
         }
 
-        // const query = new ParameterizedQuery({
-        //     text: p
-        // })
-
-        // try {
-        //     await this.conn.none(query);
-        // } catch(err) {
-        //     console.error("Error while inserting user into database!");
-        //     console.error(err.message);
-        //     console.error(err.code);
-        //     return false;
-        // }
-
-        return {};
+        return result;
     }
 
-    async delete(user) {
+    async getCountOfUsers() {
+        let query = 'SELECT count(*) AS count FROM users';
+        let result;
+    
+        try {
+            result = await this.conn.one(query);
+        } catch(err) {
+            console.error('Error while retrieving count of users');
+            console.error(err.message);
+            result = null;
+        }
 
-    }
-}
+        return result;
+    }}
 
 class ProductsRepository {
     constructor(conn) {
         this.conn = conn;
     }
 
-    async getById(id) {
+    async get(id) {
+        let query = 'SELECT * FROM products WHERE id = $1';
 
+        let result;
+        try {
+            result = await this.conn.one(query, [id]);
+        } catch(err) {
+            console.error('Error while retrieving product');
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
     }
 
-    async getAll() {
+    async getPage(pageNumber, pageLength) {
+        let query = 'SELECT * FROM products ORDER BY id OFFSET $1 ROWS FETCH NEXT $2 ROWS ONLY';
+        let result;
 
+        try {
+            result = await this.conn.manyOrNone(query, [pageLength * (pageNumber - 1), pageLength]);
+        } catch(err) {
+            console.error('Error while retrieving products from database!');
+            console.error(err.message);
+            result = null;
+        }
+
+        console.log(`Fetch result: ${result}`);
+
+        return result;
     }
 
-    async getAllInCategory(categoryId) {
+    async getCountOfProducts() {
+        let query = 'SELECT count(*) AS count FROM products';
+        let result;
+    
+        try {
+            result = await this.conn.one(query);
+        } catch(err) {
+            console.error('Error while retrieving count of orders for user');
+            console.error(err.message);
+            result = null;
+        }
 
+        return result;
     }
 
-    async findByName(search) {
+    async insert(product) {
+        let query = 'INSERT INTO products(name, description, price) VALUES ($1, $2, $3)'
 
+        let result;
+        try {
+            result = await this.conn.none(query, [product.name, product.description, product.price]);
+        } catch(err) {
+            console.error("Error while inserting new product!");
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
     }
+
+    async update(productId, product) {
+        let query = 'UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4 RETURNING *';
+
+        let result;
+        try {
+            result = await this.conn.one(query, [product.name, product.description, product.price, productId]);
+        } catch(err) {
+            console.error("Error while updating product!");
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
+    }
+
+    async delete(productId) {
+        let query = 'DELETE FROM products WHERE id = $1 RETURNING name';
+
+        let result;
+        try {
+            result = await this.conn.one(query, [productId]);
+        } catch(err) {
+            console.error("Error while updating product!");
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
+    }
+
 }
 
 class CategoriesRepository {
@@ -153,7 +220,7 @@ class OrdersRepository {
         return result;
     }
 
-    async getPage(userId, pageNumber, pageLength) {
+    async getPageForUser(userId, pageNumber, pageLength) {
         let query = 'SELECT * FROM orders WHERE user_id = $1 ORDER BY date OFFSET $2 ROWS FETCH NEXT $3 ROWS ONLY';
         let result;
 
@@ -170,6 +237,22 @@ class OrdersRepository {
         return result;
     }
 
+
+    async getPage(pageNumber, pageLength) {
+        let query = 'SELECT * FROM orders ORDER BY date OFFSET $1 ROWS FETCH NEXT $2 ROWS ONLY';
+        let result;
+
+        try {
+            result = await this.conn.manyOrNone(query, [pageLength * (pageNumber - 1), pageLength]);
+        } catch(err) {
+            console.error('Error while retrieving orders from database!');
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
+    }
+
     async getCountForUser(userId) {
         let query = 'SELECT count(*) AS count FROM orders WHERE user_id = $1';
         let result;
@@ -182,7 +265,20 @@ class OrdersRepository {
             result = null;
         }
 
-        // console.log(JSON.stringify(result));
+        return result;
+    }
+
+    async getCount() {
+        let query = 'SELECT count(*) AS count FROM orders';
+        let result;
+
+        try {
+            result = await this.conn.one(query);
+        } catch(err) {
+            console.error('Error while retrieving count of orders for user');
+            console.error(err.message);
+            result = null;
+        }
 
         return result;
     }
@@ -207,11 +303,34 @@ class OrdersProductsRepository {
     }
 }
 
+class AdminsRepository {
+    constructor(conn) {
+        this.conn = conn;
+    }
+
+    async get(username) {
+        const query = 'SELECT * FROM admins WHERE username=$1';
+
+        var result;
+        try {
+            result = await this.conn.oneOrNone(query, [username]);
+        } catch(err) {
+            console.error('Error while retrieving admin from database!');
+            console.error(err.message);
+            result = null;
+        }
+
+        return result;
+    }
+}
+
 module.exports = function(conn) {
     return {
         UserRepository: new UserRepository(conn),
         CategoriesRepository: new CategoriesRepository(conn),
         OrdersRepository: new OrdersRepository(conn),
-        OrdersProductsRepository: new OrdersProductsRepository(conn)
+        OrdersProductsRepository: new OrdersProductsRepository(conn),
+        ProductsRepository: new ProductsRepository(conn),
+        AdminsRepository: new AdminsRepository(conn)
     }
 }
