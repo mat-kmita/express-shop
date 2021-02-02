290e096951d053bea4ddf9d0688e822404bf0fb4
Mateusz Kmita <mateusz.kmita@mail.com>
2021-02-02 16:48:21 +0100

    Changed to db to run on heroku

--- db/index.js
+++ db/index.js
@@ -1,14 +1,15 @@
 const pgp = require('pg-promise')();
 
-const connectionDetails = {
-    username: 'mateusz',
-    password: 'abcd900',
-    host: 'localhost',
-    port: '5432',
-    database: 'mateusz'
-}
+// const connectionDetails = {
+//     username: 'mateusz',
+//     password: 'abcd900',
+//     host: 'localhost',
+//     port: '5432',
+//     database: 'mateusz'
+// }
 
-const db = pgp(`postgres://${connectionDetails.username}:${connectionDetails.password}@${connectionDetails.host}:${connectionDetails.port}/${connectionDetails.database}`);
+// const db = pgp(`postgres://${connectionDetails.username}:${connectionDetails.password}@${connectionDetails.host}:${connectionDetails.port}/${connectionDetails.database}`);
+const db = pgp(process.env.DATABASE_URL);
 
 async function initializeDatabase() {
     const queryFile = new pgp.QueryFile('./db/sql/create_database.sql');
